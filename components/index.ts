35265fc7c1d50f70b999dd0c904aa7b120e0677c
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-21 23:26:35 +1000

    added footer and navbar

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,10 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
 export { 
     Hero,
-    CustomButton
+    CustomButton,
+    Footer,
+    NavBar
  };
