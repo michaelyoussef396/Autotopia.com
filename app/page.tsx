8bec83da56ca64bd5c6d8f63f0061d27bb43d058
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-23 00:06:42 +1000

    created cardetails components but need to find free car api photos

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
