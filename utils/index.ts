7b622eb99da86789ce0b15378c2fc8ab072be994
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-22 23:24:18 +1000

    created fetchcars utils function

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': '30774440b2msh0a3a82bcfd5e4e3p17e3e7jsnae47c15d7573',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+
+}
