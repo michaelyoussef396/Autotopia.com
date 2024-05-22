5503aa861b563c459b84d1bf249ba0001cfdc2c5
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-22 23:17:50 +1000

    created car catalouge layout

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,15 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 export { 
     Hero,
     CustomButton,
     Footer,
-    NavBar
+    NavBar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
  };
